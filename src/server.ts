0a711dda1ed67f1e4047995275c514a509c43d79
Keni-noir <keninoir03@example.com>
2026-03-05 09:18:56 +0000

    frontend link

--- src/server.ts
+++ src/server.ts
@@ -13,7 +13,7 @@ config();
 const app = express();
 connectDB()
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://bookstore-frontend-amber.vercel.app/login"],
     credentials: true,
 }))
 
